b284253474cd69bbce34377bb65a93a49d6ce1de
ayanshaikh0525 <ayanshaikh0525@gmail.com>
2026-03-18 16:01:54 +0530

    Update clientConfig.ts

--- nextjs-frontend/lib/clientConfig.ts
+++ nextjs-frontend/lib/clientConfig.ts
@@ -4,7 +4,7 @@ const configureClient = () => {
   const baseURL = process.env.NEXT_PUBLIC_API_BASE_URL;
   console.log("BASE URL:", baseURL);
   client.setConfig({
-    baseURL: baseURL,
+    baseURL: "http://localhost:8000",
   });
 };
 
