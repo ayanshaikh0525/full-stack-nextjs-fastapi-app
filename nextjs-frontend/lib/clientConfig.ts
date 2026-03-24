ab08ef0a359ca69c164a39d8aca331da49cb42f6
ayanshaikh0525 <ayanshaikh0525@gmail.com>
2026-03-24 19:19:58 +0530

    Update clientConfig.ts

--- nextjs-frontend/lib/clientConfig.ts
+++ nextjs-frontend/lib/clientConfig.ts
@@ -2,7 +2,7 @@ import { client } from "@/app/openapi-client/client.gen";
 
 const configureClient = () => {
   const baseURL = process.env.NEXT_PUBLIC_API_URl;
-  console.log("BASE URL:", baseURL);
+  console.log("BASE URL: ", baseURL);
   client.setConfig({
     baseURL: baseURL,
   });
